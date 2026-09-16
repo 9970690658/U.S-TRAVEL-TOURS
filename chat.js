3065f9a49a7eed1d4dbbd08c658ee53bb3f431f9
Aamij Sutar <amijsutar456@gmail.com>
2026-09-16 21:31:38 +0530

    Update chat.js

--- chat.js
+++ chat.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const router = express.Router();
 
-const db = require("./database");
+const { db } = require("./database");
 const { requireAuth, requireAdmin } = require("./auth");
 
 const { Pool } = require("pg");
